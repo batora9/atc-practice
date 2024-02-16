94b647c07a57d8ce9583889670c402b1aecab593
batora <batora@DESKTOP-MainPC>
2024-02-16 22:24:32 +0900

    20240216

---
+++ Replacing Integer.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#if __has_include(<atcoder/all>)
+#include <atcoder/all>
+using namespace atcoder;
+#endif
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using P = pair<int,int>;
+using vi = vector<int>;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+
+int main(){
+    ll n,k; cin >> n >> k;
+    cout << min(n%k,k-n%k) << endl;
+    return 0;
+}
