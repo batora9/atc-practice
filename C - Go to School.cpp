08cae0cf03d4e0b3ba9d479866a55cf41fd9bea1
batora9 <sm.net_minecraft.hacker@outlook.jp>
2024-05-04 20:00:00 +0900

    20240504

--- C - Go to School.cpp
+++ C - Go to School.cpp
@@ -11,6 +11,13 @@ using vi = vector<int>;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 
 int main(){
-    
+    ll n; cin >> n;
+    vector<ll> a(n),b(n);
+    rep(i,n) cin >> a[i];
+    rep(i,n) {
+        b[a[i]-1] = i+1;
+    }
+    rep(i,n) cout << b[i] << " ";
+    cout << endl;
     return 0;
 }
