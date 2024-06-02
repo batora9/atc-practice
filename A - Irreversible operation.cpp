7d02986034feff1a8cad88eb494903867dbd1b7d
batora9 <sm.net_minecraft.hacker@outlook.jp>
2024-06-02 19:39:37 +0900

    20240602

---
+++ A - Irreversible operation.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#if __has_include(<atcoder/all>)
+#include <atcoder/all>
+using namespace atcoder;
+#endif
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using P = pair<int,int>;
+using vi = vector<int>;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+
+int main(){
+    string s; cin >> s;
+    ll cnt = 0, ans = 0;
+    rep(i,s.size()) {
+        if(s[i] == 'B') cnt++;
+        if(s[i] == 'W') ans += cnt;
+    }
+    cout << ans << endl;
+    return 0;
+}
